5faa8809c189028248ff71922907d57a0de22bd4
Zarina <abaevazarina@yandex.ru>
2022-09-20 01:11:21 +0300

    task_41 solved

--- task_41/Program.cs
+++ task_41/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int[] GenerateArray(int length)
+{
+  int[] array = new int[length]; 
+  
+  for (int i = 0; i < array.Length; i++)
+{
+  array[i] = Convert.ToInt32(Console.ReadLine());
+}
+  return array;
+}
+
+int MoreNullArray(int[] arr)
+{
+  int sum = 0;
+  for (int i = 0; i < arr.Length; i++)
+  {
+    if(arr[i] > 0)
+      sum = sum + 1;
+  }
+  return sum;
+}
+void PrintArray(int[] arr)
+{
+  for (int i = 0; i < arr.Length; i++)
+  {
+    Console.Write($"{arr[i]} ");
+  }
+  Console.WriteLine();
+}
+int M = Prompt("Введите длину массива и заполните массив: ");
+int[] arr1 = GenerateArray(M);
+PrintArray(arr1);
+int result = MoreNullArray(arr1);
+Console.Write($"Вы ввели {result} чисел(число, числа) больше нуля");
